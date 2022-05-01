9b6d7445af85aa99f784d794fa932ecd185e467a
Simon Reinisch <simon@reinisch.io>
2022-05-01 16:15:27 +0200

    refactor(core): remove unused useOutOfElementClick composable

--- src/composables/index.ts
+++ src/composables/index.ts
@@ -1,7 +1,6 @@
 export * from './useAppElement';
 export * from './useIntristicSize';
 export * from './useMutationObserver';
-export * from './useOutOfElementClick';
 export * from './useResizeObserver';
 export * from './useScrollShadow';
 export * from './useThemeStyles';
