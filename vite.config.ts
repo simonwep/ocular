72b8e3d5946874fcefda61689115cc26120ce9a8
Simon Reinisch <simon@reinisch.io>
2022-05-03 19:00:48 +0200

    feat(setup): mangle css-classes in production

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,24 @@ import {VitePWA} from 'vite-plugin-pwa';
 import {defineConfig} from 'vite';
 import manifest from './assets/manifest.json';
 
-export default defineConfig({
+export default defineConfig(({mode}) => ({
+  envPrefix: ['OAUTH'],
+
+  css: {
+    modules: {
+      generateScopedName: mode === 'development' ? '[name]_[local]_[hash:base64:5]' : (() => {
+        let index = 0;
+        return () => {
+          const hash = (index++).toString(36);
+          return hash[0].match(/^\d/) ? `_${hash}` : hash;
+        };
+      })()
+    }
+  },
+
   plugins: [
-    tsconfigPaths({ loose: true }),
+    tsconfigPaths({loose: true}),
     vue(),
-    VitePWA({ manifest })
-  ],
-  envPrefix: [
-    'OAUTH'
+    VitePWA({manifest})
   ]
-});
+}));
