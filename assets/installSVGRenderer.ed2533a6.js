f1ae8e3264548bd6496816ee01fb01e6a0239822
Simonwep <simon@reinisch.io>
2022-06-17 16:15:32 +0000

    Deploying to gh-pages from @ Simonwep/ocular@f175c37757cc35473a06981741c93b1237df8049 🚀

--- assets/installSVGRenderer.ed2533a6.js
+++ assets/installSVGRenderer.ed2533a6.js
@@ -1,4 +1,4 @@
-import{u as Ny}from"./useResizeObserver.2ed96912.js";import{d as By,c as Fy,r as zy,E as Gy,w as Go,t as Vy,h as Hy,o as Wy,j as Uy,n as $y,b as Yy}from"./index.8599440c.js";/*! *****************************************************************************
+import{u as Ny}from"./useResizeObserver.d86217d1.js";import{d as By,c as Fy,r as zy,E as Gy,w as Go,t as Vy,h as Hy,o as Wy,j as Uy,n as $y,b as Yy}from"./index.4a835b71.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
