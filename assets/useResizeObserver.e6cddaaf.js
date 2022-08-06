161cdad9e6e5196a12e10cbf8cb7ddc6315d7fd2
Simonwep <simon@reinisch.io>
2022-08-06 15:29:04 +0000

    Deploying to gh-pages from @ Simonwep/ocular@619fe74390c3864586dfa6665128e40a9ab9dc8f 🚀

--- assets/useResizeObserver.e6cddaaf.js
+++
@@ -1,1 +0,0 @@
-import{t as n,D as o}from"./index.959d0cb1.js";const i=s=>{const t=n(new DOMRect),r=new ResizeObserver(e=>{t.value=e[0].target.getBoundingClientRect()});return o(s,e=>{r.disconnect(),e&&r.observe(e)}),t};export{i as u};
