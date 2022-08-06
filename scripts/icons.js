619fe74390c3864586dfa6665128e40a9ab9dc8f
Simon Reinisch <simon@reinisch.io>
2022-08-06 15:41:10 +0200

    fix(setup): fix icon generation script

--- scripts/icons.js
+++ scripts/icons.js
@@ -1,9 +1,9 @@
-import {resolve} from 'path';
-import {readdir, writeFile} from 'fs/promises';
+import { resolve } from 'path';
+import { readdir, writeFile } from 'fs/promises';
 
 (async () => {
-    const icons = resolve(process.cwd(), 'public/icons');
-    const types = resolve(process.cwd(), 'src/app/components/base/icon/Icon.types.ts');
-    const names = (await readdir(icons)).map(v => v.replace(/\.\w+/, ''));
-    await writeFile(types, `export type AppIcon = ${names.map(v => `'${v}'`).join(' | ')};`);
+  const icons = resolve(process.cwd(), 'src/icons');
+  const types = resolve(process.cwd(), 'src/app/components/base/icon/Icon.types.ts');
+  const names = (await readdir(icons)).map((v) => v.replace(/\.\w+/, ''));
+  await writeFile(types, `export type AppIcon = ${names.map((v) => `'${v}'`).join(' | ')};`);
 })();
