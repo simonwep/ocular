0d28fa1f0e94cc00bd3611519a0b6eb2204030b4
Simonwep <simon@reinisch.io>
2022-08-20 12:36:43 +0000

    Deploying to gh-pages from @ Simonwep/ocular@54857f11b71b58f68b17544a487895ac1189689f 🚀

---
+++ assets/useMonthNames.e2773dc2.js
@@ -0,0 +1,1 @@
+import{u as e,j as n}from"./index.30a8490c.js";const u=()=>{const{d:s}=e();return n(()=>{const o=[];for(let t=0;t<12;t++)o.push(s(new Date(0,t),"month"));return o})};export{u};
---
+++ assets/useResizeObserver.a4b1f370.js
@@ -0,0 +1,1 @@
+import{t as n,D as o}from"./index.30a8490c.js";const i=s=>{const t=n(new DOMRect),r=new ResizeObserver(e=>{t.value=e[0].target.getBoundingClientRect()});return o(s,e=>{r.disconnect(),e&&r.observe(e)}),t};export{i as u};
