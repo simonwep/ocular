a66eff2e3e405cd65a6f0ae9cdc3ce1513209f96
Simon Reinisch <simon@reinisch.io>
2023-12-26 17:19:21 +0100

    feat: add confirmation dialog for copy/pasting data
    closes #15

--- src/app/pages/navigation/tools/copy-paste/PasteButton.vue
+++ src/app/pages/navigation/tools/copy-paste/PasteButton.vue
@@ -1,22 +1,27 @@
 <template>
   <ContextMenuButton
     v-if="clipboard.data.value"
-    :text="
-      t('navigation.tools.copyPaste.paste', {
-        from: clipboard.data.value?.year,
-        to: state.activeYear
-      })
-    "
+    :text="t('navigation.tools.copyPaste.paste', { from, to })"
     icon="clipboard-line"
-    @click="clipboard.paste"
+    @click="paste"
   />
 </template>
 
 <script lang="ts" setup>
+import { computed } from 'vue';
 import { useI18n } from 'vue-i18n';
 import ContextMenuButton from '@components/base/context-menu/ContextMenuButton.vue';
 import { useDataStore } from '@store/state';
 
 const { clipboard, state } = useDataStore();
 const { t } = useI18n();
+
+const from = computed(() => clipboard.data.value?.year);
+const to = computed(() => state.activeYear);
+
+const paste = () => {
+  if (window.confirm(t('navigation.tools.copyPaste.confirm', { from: from.value, to: to.value }))) {
+    clipboard.paste();
+  }
+};
 </script>
