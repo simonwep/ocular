af20cf4bc7441b47fe0cddbab8d8972bd717b1e8
Matt Dutra <thatsmattd@gmail.com>
2024-05-13 20:29:47 -0300

    feat: add brazilian portuguese language

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,11 +1,12 @@
 import { createI18n } from 'vue-i18n';
 import de from './locales/de.json';
 import en from './locales/en.json';
+import br from './locales/pt-br.json';
 
 export type MessageSchema = typeof en;
 
 const browserLocale = navigator.language.slice(0, 2).toLowerCase();
-const messages: Record<string, MessageSchema> = { en, de } as const;
+const messages: Record<string, MessageSchema> = { en, de, br } as const;
 
 export const availableLocales = Object.keys(messages);
 export const initialLocale = availableLocales.includes(browserLocale) ? browserLocale : 'en';
