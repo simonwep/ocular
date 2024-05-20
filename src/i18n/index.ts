3765a8a04ab62c5773e4a5c3df316031a20b6aea
Simon Reinisch <simon@reinisch.io>
2024-05-20 13:59:20 +0200

    refactor: remove number-formats from internationalization files

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,4 +1,4 @@
-import { createI18n } from 'vue-i18n';
+import { createI18n, IntlNumberFormat } from 'vue-i18n';
 import de from './locales/de.json';
 import en from './locales/en.json';
 import br from './locales/pt-br.json';
@@ -14,11 +14,22 @@ export const initialLocale = availableLocales.includes(browserLocale) ? browserL
 
 export type AvailableLocale = keyof typeof messages;
 
+const numberFormats: IntlNumberFormat = {
+  currency: {
+    minimumFractionDigits: 0,
+    style: 'currency'
+  },
+  percent: {
+    maximumFractionDigits: 0,
+    style: 'percent'
+  }
+};
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const i18n = createI18n({
   legacy: false,
   fallbackLocale: 'en',
   locale: initialLocale,
   messages: messages as any,
-  numberFormats: Object.fromEntries(Object.entries(messages).map((v) => [[v[0]], v[1]._numberFormats]))
+  numberFormats: Object.fromEntries(availableLocales.map((locale) => [locale, numberFormats]))
 });
