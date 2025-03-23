b80ea4d1bb65b384ccd3cd3ae40a3151de236d39
Simon Reinisch <simon@reinisch.io>
2025-03-23 09:03:04 +0100

    feat: add french localizations
    courtesy of @AnthonyDomotique
    closes #97

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,6 +2,7 @@ import cze from './locales/cze.json?url';
 import de from './locales/de.json?url';
 import en from './locales/en.json?url';
 import es from './locales/es.json?url';
+import fr from './locales/fr.json?url';
 import hu from './locales/hu.json?url';
 import it from './locales/it.json?url';
 import pl from './locales/pl.json?url';
@@ -11,7 +12,7 @@ import tr from './locales/tr.json?url';
 
 import { createI18n, IntlNumberFormat } from 'vue-i18n';
 
-const localeUrls = { de, en, tr, it, 'pt-br': ptbr, cze, hu, pl, ru, es };
+const localeUrls = { de, en, tr, it, 'pt-br': ptbr, cze, hu, pl, ru, es, fr };
 
 export const availableLocales = Object.keys(localeUrls);
 
