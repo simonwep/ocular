2391d397c68bda828e7fbc263f58a57a8335e324
Simon Reinisch <simon@reinisch.io>
2025-04-27 18:15:31 +0200

    test: ignore service-worker and playwright related errors in e2e tests

--- test/utils/assertNoErrors.ts
+++ test/utils/assertNoErrors.ts
@@ -1,11 +1,20 @@
 import { Page } from '@playwright/test';
 
+// Service worker are blocked during testing, so we need to ignore them
+const IGNORE_REGEX = /playwright|service-worker/i;
+
 export const assertNoErrors = (page: Page) => {
   page.on('console', (msg) => {
+    const text = msg.text();
+
+    if (IGNORE_REGEX.test(text)) {
+      return;
+    }
+
     if (msg.type() === 'error') {
-      throw new Error(`Console error: ${msg.text()}`);
+      throw new Error(`Console error: ${text}`);
     } else if (msg.type() === 'warning') {
-      throw new Error(`Console warning: ${msg.text()}`);
+      throw new Error(`Console warning: ${text}`);
     }
   });
 };
