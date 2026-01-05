b43baf3beb778ed3081dddcf83210427bac7652b
Simon Reinisch <simon@reinisch.io>
2026-01-05 06:54:11 +0100

    refactor: polyfill uuid properly

--- src/utils/uuid/uuid.ts
+++ src/utils/uuid/uuid.ts
@@ -1,1 +1,5 @@
-export const uuid = (): string => crypto.randomUUID?.() ?? (Math.random() * 1e17).toString(32);
+export const uuid = (): string =>
+  crypto.randomUUID?.() ??
+  '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) =>
+    (+c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(16)
+  );
